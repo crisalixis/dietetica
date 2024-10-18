b0e90e942f7212cb46a70c5085a87ca4ad0a38c6
crisalixis <164426470+crisalixis@users.noreply.github.com>
2024-10-18 19:28:12 -0300

    Add files via upload

--- dietetica/includes/header.php
+++ dietetica/includes/header.php
@@ -6,13 +6,18 @@ if(empty($_SESSION['active'])) {
 
 ?>
 <header>
-		<div class="header-1">
-			<h1>Sistema Facturación</h1>
-                <div class="optionsBar">
-                    <span class="user"><?php echo $_SESSION['user'].' -'.$_SESSION['rol'] ?></span>
-                    <img class="photouser" src="img/user.svg" alt="Usuario">
-                    <a href="salir.php"><img class="close" src="img/salir.svg" alt="Salir del sistema" title="Salir"></a>
-                </div>
-		</div>
+	<div class="header-1">
+	        <h1>Sistema Facturación</h1>
+                        <div class="optionsBar">
+                        <span class="user"><?php echo $_SESSION['user'].' -'.$_SESSION['rol'] ?></span>
+                        <img class="photouser" src="img/user.svg" alt="Usuario">
+                        <a href="salir.php"><img class="close" src="img/salir.svg" alt="Salir del sistema" title="Salir"></a>
+                        </div>
+	</div>
         <?php include "nav.php" ?>
 </header>
+<div class="modal">
+       <div class="bodyModal">
+                
+       </div> 
+</div>
