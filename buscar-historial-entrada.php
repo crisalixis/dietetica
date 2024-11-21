1e02bdd65b53fb256d23f2890e3e40b6caa1f7a2
crisalixis <164426470+crisalixis@users.noreply.github.com>
2024-11-21 16:08:25 -0300

    nuevo archivo

---
+++ buscar-historial-entrada.php
@@ -0,0 +1,98 @@
+<?php
+    session_start();
+    include "config/db.php";
+?>
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+	<meta charset="UTF-8">
+    <?php include "includes/scripts.php" ?>
+    <title>Lista de Proveedores</title>
+</head>
+<body>
+    <?php include "includes/header.php" ?>
+	<section id="container">
+		
+        <h1><i class="fas fa-cubes"></i> Historial de entradas</h1>
+        <form action="buscar-historial-entrada.php" method="get" class="form-search">
+            <input type="text" name="busqueda" id="busqueda" placeholder="Buscar">
+            <button type="submit"class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+        <table>
+            <tr>
+                <th>Correlativo</th>
+                <th>Producto</th>
+                <th>Fecha</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+            </tr>
+            <?php 
+                //Paginador
+                $sql_register = mysqli_query($conexion, "SELECT COUNT(*) as total_registro FROM entradas");
+                $result_register = mysqli_fetch_array($sql_register);
+                $total_registro = $result_register['total_registro'];
+
+                $por_pagina = 9;
+                
+                if(empty($_GET['pagina'])){
+                    $pagina = 1;
+                }else{
+                    $pagina = $_GET['pagina'];
+                }
+
+                $desde = ($pagina - 1) * $por_pagina;
+                $total_paginas = ceil($total_registro / $por_pagina);
+
+
+                $query = mysqli_query($conexion, "SELECT e.correlativo, p.codproducto, e.fecha, e.cantidad, e.precio FROM entradas e INNER JOIN producto p ON e.codproducto = p.codproducto ORDER BY e.correlativo ASC LIMIT $desde, $por_pagina");
+                $result = mysqli_num_rows($query);
+
+                if($result > 0){
+                    while ($data = mysqli_fetch_array($query)){
+                        $formato = 'Y-m-d H:i:s';
+                        $fecha = DateTime::createFromFormat($formato, $data["fecha"]);
+                    ?>
+                        <tr>
+                            <td><?php echo $data['correlativo'] ?></td>
+                            <td><?php echo $data['codproducto'] ?></td>
+                            <td><?php echo $fecha->format('d-m-Y')?></td>
+                            <td><?php echo $data['cantidad'] ?></td>
+                            <td><?php echo $data['precio'] ?></td>
+                        </tr>
+                <?php
+                    }
+                }
+                ?>
+        </table>
+        <div class="paginador">
+            <ul>
+                <?php
+                    if($pagina != 1){
+
+                ?>
+                <li><a href="?pagina=<?php echo 1 ?>"><i class="fas fa-backward-step"></i></a></li>
+                <li><a href="?pagina=<?php echo $pagina - 1 ?>"><i class="fas fa-angles-left"></i></a></li>
+                <?php                        
+                }   
+
+                    for ($i=1; $i <= $total_paginas; $i++){
+                        if($i == $pagina){
+                            echo '<li class="pageSelected">'.$i.'</li>';
+                        }else{
+                            echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
+                        }
+                    }
+                
+                    if($pagina != $total_paginas){
+
+                ?>
+                
+                <li><a href="?pagina=<?php echo $pagina + 1 ?>"><i class="fas fa-angles-right"></i></a></li>
+                <li><a href="?pagina=<?php echo $total_paginas ?>"><i class="fas fa-forward-step"></i></a></li>
+                <?php } ?>
+            </ul>
+        </div>
+	</section>
+	<?php include "includes/footer.php" ?>
+</body>
+</html>
